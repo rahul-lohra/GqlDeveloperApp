ef119ac8cccfd27a3472b838696e56bd9899c952
Rahul Kumar Lohra <rahul.lohra@tokopedia.com>
2020-08-31 05:15:12 +0530

    update to fail test case

--- features/promo/src/androidTest/java/com/rahullohra/promo/Module2InstrumentedTest.kt
+++ features/promo/src/androidTest/java/com/rahullohra/promo/Module2InstrumentedTest.kt
@@ -19,6 +19,6 @@ class Module2InstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.rahullohra.promo.test", appContext.packageName)
+        assertEquals("com.rahullohra.promo.test1", appContext.packageName)
     }
 }
